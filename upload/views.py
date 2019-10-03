326ac09e0655246f76fe3a09c63d2aa4e035a693
andy.huang <xxxxsars@gmail.com>
2019-10-03 14:32:17 +0800

    Handle uploaded script file

--- upload/views.py
+++ upload/views.py
@@ -5,6 +5,7 @@ from upload.models import *
 import os
 import zipfile
 import platform
+import shutil
 
 # Create your views here.
 
@@ -42,6 +43,15 @@ def upload_index(request):
 
             handle_uploaded_file(request.FILES['file'],task_name)
 
+            upload_list= uploaded_list(task_name)
+
+
+
+            if len(upload_list)>0:
+                error_message = "Your files %s had been uploaded!"%",".join(upload_list)
+                return render(request, "upload.html", locals())
+
+            save_tmp_file(task_name)
 
             up = Upload_TestCase.objects.create(task_id=task_id,   task_name=task_name,description = task_descript,  script_name=script_name)
 
@@ -53,10 +63,6 @@ def upload_index(request):
 
             susessful = "Upload  Test Case ID: [ %s ] was successfully!" % task_id
             return render(request, "upload.html", locals())
-
-        else:
-
-            print(a.errors,u.errors)
     else:
         a = ArgumentForm()
         u = UploadFileForm()
@@ -66,18 +72,10 @@ def upload_index(request):
 
     return render(request,"upload.html",locals())
 
-
-
-
-
 def get_serial_number(task_id):
-
-
     datas = Upload_TestCase.objects.filter(task_id__iregex=r"^%s\d{2}"%task_id).values()
-
     if len(datas) == 0:
         return "00"
-
     serials = []
     for data in datas:
         serial = re.search(r'(\d{2})$', data["task_id"]).group(1)
@@ -93,16 +91,16 @@ def get_serial_number(task_id):
 
     return max_serial
 
-def handle_uploaded_file(f,script_name):
+def handle_uploaded_file(f,task_name):
     path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     if platform.system() == "Windows":
-        source_zip = path + r'\upload_folder\\' + f.name
-        unzip_path = path + r'\upload_folder\\'
+        source_zip = path + r'\upload_folder_tmp\\' + f.name
+        unzip_path = path + r'\upload_folder_tmp\\'
 
     else:
-        source_zip = path + '/upload_folder/' + f.name
-        unzip_path = path + '/upload_folder/'
+        source_zip = path + '/upload_folder_tmp/' + f.name
+        unzip_path = path + '/upload_folder_tmp/'
 
 
     with open(source_zip, 'wb+') as destination:
@@ -112,7 +110,82 @@ def handle_uploaded_file(f,script_name):
 
 
     with zipfile.ZipFile(source_zip, 'r') as zip_ref:
-        zip_ref.extractall(unzip_path+script_name)
+        zip_ref.extractall(unzip_path+task_name)
 
     # remove zip file
     os.remove(source_zip)
+
+def save_tmp_file(task_name):
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    if platform.system() == "Windows":
+        dest_path = path + r'\upload_folder\\' + task_name
+        source_path =  path + r'\upload_folder_tmp\\' + task_name
+    else:
+        dest_path = path + '/upload_folder/' + task_name
+        source_path =  path + '/upload_folder_tmp/' + task_name
+
+    print(source_path,dest_path)
+    shutil.move(source_path,dest_path)
+
+def task_files():
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+    task_list = []
+    for task in Upload_TestCase.objects.all():
+        task_list.append(task.task_name)
+
+
+    task_files ={}
+    for task_name in task_list:
+        if platform.system() == "Windows":
+            file_path = path + r'\upload_folder\\' + task_name
+        else:
+            file_path = path + '/upload_folder/' + task_name
+
+
+        # add source pyfile
+        file_list = []
+        for root, folders, files in os.walk(file_path):
+
+            for sfile in files:
+                aFile = os.path.join(root, sfile)
+                file_list.append(os.path.relpath(aFile, file_path))
+
+        task_files[task_name] = file_list
+
+    return task_files
+
+def uploaded_list(new_task_name):
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    if platform.system() == "Windows":
+        tmp_upload_path = path + r'\upload_folder_tmp\\' + new_task_name
+
+    else:
+        tmp_upload_path = path + '/upload_folder_tmp/' +new_task_name
+
+
+    file_map = task_files()
+
+    new_files = []
+    for root, folders, files in os.walk(tmp_upload_path):
+        for sfile in files:
+            aFile = os.path.join(root, sfile)
+            new_files.append(os.path.relpath(aFile, tmp_upload_path))
+
+
+    upload_list = []
+    for _, fs in file_map.items():
+        for f in fs:
+            if f in new_files:
+                upload_list.append(f)
+
+    if len(upload_list)>0:
+        try:
+            shutil.rmtree(tmp_upload_path)
+        except Exception:
+            pass
+
+
+    return upload_list
