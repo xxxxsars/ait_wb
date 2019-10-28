5368a6380b840bb7a0c2f44f513955cd31967441
andy.huang <xxxxsars@gmail.com>
2019-10-28 13:39:38 +0800

    Fix modify test script the sample not showing error

--- test_script/update/forms.py
+++ test_script/update/forms.py
@@ -4,29 +4,6 @@ import zipfile
 from common.limit import input_script_name, input_zip_file_name,input_task_name
 
 
-class QueryTestCaseForm(forms.Form):
-    task_id = forms.CharField(max_length=2551, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    task_name = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
-    def clean_task_id(self):
-        task_id = self.cleaned_data['task_id']
-        self.task_id = task_id
-        if task_id != "":
-            if Upload_TestCase.objects.filter(task_id=task_id).count() <= 0:
-                raise forms.ValidationError("Your provide task id : [%s] is no on record,please upload it." % task_id)
-
-    def clean_task_name(self):
-
-        task_name = self.cleaned_data['task_name']
-        self.task_name = task_name
-        if task_name != "":
-            if Upload_TestCase.objects.filter(task_name=task_name).count() <= 0:
-                raise forms.ValidationError(
-                    "Your provide TestCase Name : [%s] is no on record,please upload it." % task_name)
-
-    def clean(self):
-        if self.task_id == "" and self.task_name == "":
-            raise forms.ValidationError("Your must be provide ID or Script Name to inquire data.")
 
 
 class UpdateFileForm(forms.Form):
@@ -72,7 +49,6 @@ class UpdateFileForm(forms.Form):
                 'Your Script Name suffix must contains ".py" and name only allow number, letter and underline.')
 
     def clean_file(self):
-
         file = self.cleaned_data.get("file", False)
         if file != None:
             if input_zip_file_name.search(str(file)) == None:
