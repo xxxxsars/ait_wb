b6b47b4e869c1c54d9db2e7efc7111c5140b839e
andy.huang <xxxxsars@gmail.com>
2020-03-10 11:20:21 +0800

    Fix project list of the last upload time

--- project/views.py
+++ project/views.py
@@ -100,8 +100,10 @@ def list_project(request):
         project_name = p.project_name
 
         upload_date = "Not Uploaded"
-        if Project_Upload_time.objects.filter(project_name=p).exists():
-            upload_date = Project_Upload_time.objects.filter(project_name=p).last().time
+
+        filter_instances = [ instance  for instance in  Project_Upload_time.objects.filter(project_name=p)  if instance.had_upload ]
+        if len(filter_instances) >0:
+            upload_date = filter_instances[-1].time
 
         project_dict = {"project_id": 'prj_%d' % prj_id, "project_name": p.project_name,
                         "owner_user": p.owner_user.username, "date": p.time, "upload_date": upload_date}
